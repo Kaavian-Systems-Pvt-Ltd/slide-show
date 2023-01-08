cd27f9bae8048c49a1070f73e27be6813a46fa7d
kokila <kokila.ss@kaaviansys.com>
2023-01-08 22:18:45 +0530

    correction based on review

--- src/imageslider/slider.jsx
+++ src/imageslider/slider.jsx
@@ -1,38 +1,47 @@
 import React , { useState , useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Dot } from './dot';
-import { Arrows } from './arrow';
-import { SliderContent } from './slidercontent';
+import { AiOutlineArrowLeft , AiOutlineArrowRight } from 'react-icons/ai';
+
 import './slider.css';
 
 /**
- * 
+ *
  * @param {Object} props
  * @param {Array.<{url: String, caption: String}>} props.slides
  * @param {String} props.width width of slider in percentage
  * @param {String} props.height height of the slider in pixels
- * @param {Boolean} props.autoPlay if it false we have to move slide by arrows 
+ * @param {Boolean} props.autoPlay if it false we have to move slide by arrows
  * @param {Boolean} props.showBullets it shows which imsge is active
  * @param {Boolean} props.showArrow used to move a slide if autoPlay is stop
  * @param {Number} props.transitionSpeed In milliSeconds
  * @returns slideshow
- * 
+ *
  */
 
 export const Slider = (props)=> {
   const [ activeIndex , setActiveIndex ] = useState (0);
-  // const [ arrow , setArrow ] = useState (props.showArrow);
-  const { length } = props.slides;
+  const length = props.slides.length;
   useEffect (()=> {
     if (!props.slides) return null;
-    // if (props.autoPlay !== true) setArrow (true);
-    // else setArrow (false);
-    props.autoPlay && setTimeout (()=> {
-      activeIndex === length - 1
-        ? setActiveIndex (0)
-        : setActiveIndex (activeIndex + 1);
-    } , props.transitionSpeed);
+
+    props.autoPlay
+      && setTimeout (()=> {
+        activeIndex === length - 1
+          ? setActiveIndex (0)
+          : setActiveIndex (activeIndex + 1);
+      } , props.transitionSpeed);
   });
+  const prevSlide = ()=> {
+    activeIndex === 0
+      ? setActiveIndex (length - 1)
+      : setActiveIndex (activeIndex - 1);
+  };
+  const nextSlide = ()=> {
+    activeIndex === length - 1
+      ? setActiveIndex (0)
+      : setActiveIndex (activeIndex + 1);
+  };
 
   const setSize = {
     'width' : props.width ,
@@ -41,24 +50,30 @@ export const Slider = (props)=> {
 
   return (
     <div className='slider-container' style={setSize}>
-      <SliderContent
-        activeIndex={activeIndex}
-        slide={props.slides}
-      />
-      {props.showArrow && (
-      <Arrows
-        prevSlide={()=> (activeIndex === 0 ? setActiveIndex (length - 1) : setActiveIndex (activeIndex - 1))}
-        nextSlide={()=> (activeIndex === length - 1 ? setActiveIndex (0) : setActiveIndex (activeIndex + 1))}
-      />
-      )}
-      {props.showBullets && (
-      <Dot
-        activeIndex={activeIndex}
-        onclick={(activeIndex)=> setActiveIndex (activeIndex)}
-        sliderimages={props.slides}
-      />
-      )}
+      <AiOutlineArrowLeft className='arrow-prev' onClick={prevSlide} />
+      <AiOutlineArrowRight className='arrow-next' onClick={nextSlide} />
+      {props.slides.map ((slide , index)=> {
+        return (
+          <div
+            className={index === activeIndex ? 'slide current' : 'slide'}
+            key={index}
+          >
+            {index === activeIndex && (
+              <>
+                <img src={slide.url} alt='slide' />
+              </>
+            )}
+          </div>
+        );
+      })}
 
+      {props.showBullets && (
+        <Dot
+          activeIndex={activeIndex}
+          onclick={(activeIndex)=> setActiveIndex (activeIndex)}
+          sliderimages={props.slides}
+        />
+      )}
     </div>
   );
 };
@@ -75,8 +90,8 @@ Slider.propTypes = {
 Slider.defaultProps = {
   'autoPlay' : true ,
   'transitionSpeed' : 3000 ,
-  'width' : '70%' ,
-  'height' : '400px' ,
+  'width' : '50%' ,
+  'height' : '50vh' ,
   'showBullets' : true ,
   'showArrow' : false
 };
